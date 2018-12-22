bce1049f57ff11e6744599bbfa81a0096e45e26b
Onuma <ohnhistory@gmail.com>
2018-12-22 22:46:59 +0900

    lib側でlodashをいれているため、明記しなくてもlodashが使えることを確認

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const mylib = require('mylib');
 const url = mylib.testJoin('a', 'b');
 console.log(url);
+
+console.log(mylib.fillArray(10, 5));
+
+const { times } = require('lodash');
+console.log(times(5));
